97b8653ee7de706925e9ded00ebb6613be4f98bf
Grace Bethany Hoober <grace.hoober66@gmail.com>
2023-07-29 12:30:22 -0700

    Initial code to remove meme

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 
 let form = document.querySelector("form");
-
+let idAttribute = 1;
 function displayMeme(e){
   e.preventDefault();
   let memeList = document.querySelector("#meme-list");
@@ -9,26 +9,49 @@ function displayMeme(e){
   let bottomText = document.querySelector("#bottomText").value;
 
   let memeDiv = document.createElement("div");
-  let imageContainer = document.createElement("img")
-  imageContainer.classList.add("photo")
+  memeDiv.classList.add("meme-content")
+  memeDiv.setAttribute("id", idAttribute.toString() + "meme")
+
+  let imageContainer = document.createElement("img");
+  imageContainer.classList.add("photo");
   imageContainer.src = image;
 
+  let topTextDiv = document.createElement("div");
+  topTextDiv.classList.add("text-above");
+  topTextDiv.append(topText)
+
+  let bottomTextDiv = document.createElement("div");
+  bottomTextDiv.classList.add("text-below");
+  bottomTextDiv.append(bottomText)
+
+  // let removeDiv = document.createElement('div');
+  // removeDiv.classList.add("remove-div");
+  // let removeText = document.createTextNode("Delete Meme")
+  // removeDiv.appendChild(removeText);
+
   memeDiv.append(imageContainer);
-  memeDiv.append(topText);
-  memeDiv.append(bottomText);
+  memeDiv.append(topTextDiv);
+  memeDiv.append(bottomTextDiv);
+  //memeDiv.append(removeDiv)
   memeList.append(memeDiv);
-}
+
+//function to remove meme from page (parent is memeList, child is memeContent)
+let memeContent = document.querySelector(".meme-content");
+memeContent.addEventListener("click", function(){
+  memeList.removeChild(memeContent)
+});
+};
 
 
 
-//function to remove meme from page
-// function removeMeme(){
+//function to clear input feilds
+function clearInputs(){
 
-// }
+};
 
 //event listeners
-form.addEventListener("submit", displayMeme)
-// memeList.addEventListener("click", removeMeme)
+form.addEventListener("submit", displayMeme);
+
 
 /*
   https://developer.mozilla.org/en-US/docs/Web/API/Element/append
@@ -45,45 +68,3 @@ form.addEventListener("submit", displayMeme)
 */
 
 
-
-
-
-
-
-
-// topText.addEventListener("change", () => {
-//   imageToCanvas(memeList,image,topText.value, bottomText.value)
-// });
-
-// bottomText.addEventListener("change", () => {
-//   imageToCanvas(memeList,image,topText.value, bottomText.value)
-// });
-
-// /*make the image show up on the page(canvas element*/
-// function imageToCanvas (canvas, img, topText, bottomText){
-//   let canvasContext = canvas.getContext("2d");
-//   const width = image.width;
-//   const height = image.height;
-//   let fontSize = Math.floor(width/10);//use width so you get a relative size vs and absolute size
-//   let spaceFromTextToEdge = height / 25;
-//   //update canvas background
-//   canvas.width = width;
-//   canvas.height =height;
-//   canvasContext.drawImage(img,0,0)
-//   //render text
-//   canvasContext.strokeStyle = "black";//color of line around letters
-//   canvasContext.lineWidth = Math.floor(fontSize / 6)// width of black line around text letters
-//   canvasContext.fillStyle = "white";
-//   canvasContext.testAlign = "center";//center the text on the image
-//   canvasContext.lineJoin = "round";
-//   canvasContext.font = `${fontSize}px arial`;//font-family
-//   //add top text
-//   canvasContext.textBaseline = "top";// helps adhere to spaceFromTextToEdge
-//   canvasContext.strokeText(topText, width/2, spaceFromTextToEdge)// parameters are the text from input for top of meme, the x axis, y axis
-//   canvasContext.fillText(topText, width/2, spaceFromTextToEdge);
-//   //add bottom text
-//   canvasContext.textBaseline = "bottom";// helps adhere to spaceFromTextToEdge
-//   canvasContext.strokeText(bottomText, width/2, height - spaceFromTextToEdge)
-//   canvasContext.fillText(bottomText, width/2, height - spaceFromTextToEdge);
-
-// }
